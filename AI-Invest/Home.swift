95ca9ec37c6926dbf3257b7a5f847ea103e7370b
Tang47 <tminghua@gmail.com>
2022-03-06 06:49:54 +0800

    设定项目目标

---
+++ AI-Invest/Home.swift
@@ -0,0 +1,14 @@
+//
+//  Home.swift
+//  AI-Invest
+//
+//  Created by 唐明华 on 2022/3/6.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
